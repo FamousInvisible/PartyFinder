011ad95d956e66045d764a7ea96d65df24fa66c7
FamousInvisible <sania.prihodko15@gmail.com>
2024-08-17 18:49:22 +0300

    bugfix: Исправлен баг с протуханием токина

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -9,9 +9,12 @@ export class ProfileService {
   http = inject(HttpClient)
 
   baseApiUrl = 'https://icherniakov.ru/yt-course/'
-  constructor() { }
 
   getTestAccounts(){
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
+  }
+
+  getMe(){
+    return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
   }
 }
