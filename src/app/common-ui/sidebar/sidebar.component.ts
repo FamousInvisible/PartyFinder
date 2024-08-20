b2bb5feaae1c1b4f51306e15543ed13261f45f57
FamousInvisible <sania.prihodko15@gmail.com>
2024-08-20 20:57:06 +0300

    fetch: Сверстана главная страница приложения

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { NgFor } from '@angular/common';
+import { SubscriberCardComponent } from "./subscriber-card/subscriber-card.component";
+import { RouterLink } from '@angular/router';
+import { ProfileService } from '../../data/services/profile.service';
+import { AsyncPipe } from '@angular/common';
+import { firstValueFrom, map } from 'rxjs';
+import { JsonPipe } from '@angular/common';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent, NgFor],
+  imports: [SvgIconComponent, NgFor, SubscriberCardComponent, RouterLink, AsyncPipe, JsonPipe, ImgUrlPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService)
+
+  subscribers$ = this.profileService.getSubscribersShortList()
+  
+  me = this.profileService.me
+
   menuItems = [
     {
       label: 'Моя страница',
@@ -27,4 +40,8 @@ export class SidebarComponent {
       link: 'search'
     }
   ]
+
+  ngOnInit(){
+    firstValueFrom(this.profileService.getMe())
+  }
 }
